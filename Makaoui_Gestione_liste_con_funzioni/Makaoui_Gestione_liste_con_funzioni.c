2c02f5c9373eeca5aed2beeb3fc627d8ca114acd
maka-6 <makaouiyouness30@gmail.com>
2024-11-08 12:23:55 +0100

    Esercitazione Gestione liste con funzioni

--- Makaoui_Gestione_liste_con_funzioni/Makaoui_Gestione_liste_con_funzioni.c
+++ Makaoui_Gestione_liste_con_funzioni/Makaoui_Gestione_liste_con_funzioni.c
@@ -1,12 +1,55 @@
 /*
  * Autore: Makaoui Youness
- * Luogo: 
+ * Luogo: Home
  * Classe: 4G
  * Data 08/11/2024
- * Description: ${DESCRIPTION}
+ * Description:
+    - Inserimento in testa
+    - Visualizzazione
+    - Cancellazione in testa
+    - Cancellazione in coda
+    - Cancellazione di tutta la lista
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {}
+typedef struct money{
+
+    double cash$$;
+    struct money *pointNext;
+
+}Money;
+
+int addMoreMoney(Money *soldi);
+
+void seeMoney(Money *soldi);
+
+int removeMoneyDown(Money *soldi);
+
+int removeMoneyUp(Money *soldi);
+
+int becomePoor(Money *soldi);
+
+int main() {
+
+    Money *soldi = NULL;
+    int c;
+    do {
+        printf("\nHello");
+        scanf("%d",&c);
+        fflush(stdin);
+        switch (c) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                break;
+        }
+    }while(c!=4);
+}
